ae99b8dc7b03b5febd041a0c667dd7ebc0e1d696
siudev <phuongnv1996@outlook.com>
2023-10-15 17:46:31 +0700

    Update app.py file

--- app.py
+++ app.py
@@ -28,10 +28,7 @@ def home():
 def predict():
     # Performs an sklearn prediction
     try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        clf = joblib.load("boston_housing_prediction.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
