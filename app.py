368a7d5210855154b27aef9f9787abd4dca7e8ce
siudev <phuongnv1996@outlook.com>
2023-10-15 17:55:58 +0700

    Fix bug app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info("Scaling Payload: %s payload")
+    LOG.info("Scaling Payload: %s", payload)
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
@@ -30,13 +30,13 @@ def predict():
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
     except:
-        LOG.info("JSON payload: %s json_payload")
+        LOG.info("JSON payload: %s", json_payload)
         return "Model not loaded"
 
     json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
+    LOG.info("JSON payload: %s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
+    LOG.info("inference payload DataFrame: %s", inference_payload)
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': prediction})
